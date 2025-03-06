cb0798ff6697c7d6c21ec5e854312c8268a155a4
chaihaojiang <chaihaojiang@15847769400@163.com>
2025-03-06 21:53:06 +0800

    fix

--- src/open_vocabulary/post_processing/postprocess.py
+++ src/open_vocabulary/post_processing/postprocess.py
@@ -221,12 +221,12 @@ def evaluate():
 # Example usage:
 if __name__ == "__main__":
     print(f"{get_nowtime()}  开始load 预测文件")
-    with open("/root/post_process_data/dino/groundingdino_product_test_prediction.json", "r") as f:
+    with open("/root/post_process_data/vild/product/vild_product_test_prediction.json", "r") as f:
         raw_preds = json.load(f)
     print(f'{get_nowtime()}  预测文件加载完毕')
     pred_coco_format = convert_custom_predictions_to_coco(raw_preds)
     print("后处理完成，将结果写入文件")
-    with open("/root/post_process_data/dino/groundingdino_product_test_prediction_coco_baseline.json", "w") as f:
+    with open("/root/post_process_data/vild/product/vild_product_test_prediction_coco_baseline.json", "w") as f:
         json.dump(pred_coco_format, f)
     # pred_coco_format = convert_custom_predictions_to_coco(raw_preds, pp_func_iter=[
     #     partial(remove_covered_boxes, threshold=0.8, type="product")])
