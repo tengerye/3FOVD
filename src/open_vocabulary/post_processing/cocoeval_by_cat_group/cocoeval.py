24815bef5e3320182f2302cdd3dc7f08b2bb64d2
chaihaojiang <chaihaojiang@15847769400@163.com>
2025-03-08 14:37:56 +0800

    修改vild 车辆运行时路径

--- src/open_vocabulary/post_processing/cocoeval_by_cat_group/cocoeval.py
+++ src/open_vocabulary/post_processing/cocoeval_by_cat_group/cocoeval.py
@@ -2,22 +2,29 @@
 import json
 import os
 import sys
+from datetime import datetime
+
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 from tqdm import tqdm
 
-def main(group_idx, detection_dir, groundtruth_path, output_path):
+def get_nowtime():
+    now = datetime.now()
+    formatted_time = now.strftime('%Y/%m/%d %H:%M:%S')
+    return formatted_time
 
+
+def main(group_idx, detection_dir, groundtruth_path, output_path):
     # 加载检测结果
+    print(f"{get_nowtime()} 数据加载")
     det_path = os.path.join(detection_dir, f'group_{group_idx}.json')
     with open(det_path, 'r') as f:
         detections = json.load(f)
-
     # 加载COCO真实标注
     coco_gt = COCO(groundtruth_path)
-
     # 加载检测结果到COCO格式
     coco_dt = coco_gt.loadRes(detections)
+    print(f"{get_nowtime()} 数据加载完成")
 
     # 获取当前组包含的类别
     cat_ids = list({d['category_id'] for d in detections})
@@ -26,7 +33,8 @@ def main(group_idx, detection_dir, groundtruth_path, output_path):
     category_aps = {}
 
     # 逐类别评估
-    for cat_id in tqdm(cat_ids):
+    for cat_id in cat_ids:
+        print(f"{get_nowtime()} 当前处理类别id为{cat_id}")
         # 初始化评估器
         coco_eval = COCOeval(coco_gt, coco_dt, 'bbox')
         coco_eval.params.catIds = [cat_id]
@@ -35,9 +43,9 @@ def main(group_idx, detection_dir, groundtruth_path, output_path):
         coco_eval.evaluate()
         coco_eval.accumulate()
         coco_eval.summarize()
-
         # 记录AP（IoU 0.5:0.95）
         category_aps[int(cat_id)] = coco_eval.stats[0].item()  # 转换为Python float
+        print(f"{get_nowtime()} 类别{cat_id}处理完成")
 
     # 保存结果
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
@@ -46,8 +54,9 @@ def main(group_idx, detection_dir, groundtruth_path, output_path):
 
 
 if __name__ == '__main__':
-    group_idx = int(sys.argv[1])
-    groundtruth_path = "/root/post_process_data/dino/vehicle/instances_vehicle_test.json"
-    detection_dir = "/root/exp/detection_group"
-    output_path = f"/root/exp/aps/group_{group_idx}_ap.json"
-    main(group_idx, detection_dir, groundtruth_path, output_path)
+    group_idx = [i for i in range(12)]  # 车辆数据集一共会跑出12个组  # int(sys.argv[1])
+    for group_id in tqdm(group_idx):
+        groundtruth_path = "/root/post_process_data/dino/vehicle/instances_vehicle_test.json"
+        detection_dir = "/root/exp/detection_group"
+        output_path = f"/root/exp/aps/group_{group_id}_ap.json"
+        main(group_id, detection_dir, groundtruth_path, output_path)
