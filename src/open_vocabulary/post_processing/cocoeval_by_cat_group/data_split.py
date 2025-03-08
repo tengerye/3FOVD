b516f17988022bbf8c5d977e04640305a14ff4d2
chaihaojiang <chaihaojiang@15847769400@163.com>
2025-03-08 13:07:28 +0800

    add print

--- src/open_vocabulary/post_processing/cocoeval_by_cat_group/data_split.py
+++ src/open_vocabulary/post_processing/cocoeval_by_cat_group/data_split.py
@@ -1,22 +1,29 @@
 
 import json
+from datetime import datetime
 from itertools import islice
 import os
 
 from tqdm import tqdm
 
-CATEGORIES_PER_GROUP = 20  # 每组类别数
+CATEGORIES_PER_GROUP = 50  # 每组类别数
 
 def chunk_indices(lst, chunk_size):
     """将列表索引分块"""
     for i in range(0, len(lst), chunk_size):
         yield list(lst[i:i + chunk_size])
 
+def get_nowtime():
+    now = datetime.now()
+    formatted_time = now.strftime('%Y/%m/%d %H:%M:%S')
+    return formatted_time
+
 def main(detection_path, output_dir):
     # 加载原始检测结果
+    print(f"{get_nowtime()} 加载原始detection文件")
     with open(detection_path, 'r') as f:
         all_detections = json.load(f)
-
+    print(f"{get_nowtime()} 原始detection文件加载结束")
     # 获取所有唯一类别ID并排序
     all_categories = sorted({d['category_id'] for d in all_detections})
 
@@ -24,7 +31,8 @@ def main(detection_path, output_dir):
     os.makedirs(output_dir, exist_ok=True)
 
     # 分块处理
-    for group_idx, cat_group in enumerate(tqdm(chunk_indices(all_categories, CATEGORIES_PER_GROUP))):
+    for group_idx, cat_group in enumerate(chunk_indices(all_categories, CATEGORIES_PER_GROUP)):
+        print(f"{get_nowtime()} start split {group_idx} group")
         # 筛选当前组的检测结果
         group_detections = [
             d for d in all_detections
@@ -36,7 +44,7 @@ def main(detection_path, output_dir):
         with open(output_path, 'w') as f:
             json.dump(group_detections, f)
 
-        print(f'Group {group_idx}: {len(cat_group)} categories, {len(group_detections)} detections')
+        print(f'{get_nowtime()} Group {group_idx}: {len(cat_group)} categories, {len(group_detections)} detections')
     pass
 
 
